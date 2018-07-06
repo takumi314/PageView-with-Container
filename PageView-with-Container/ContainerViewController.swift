44212f182eeaf469524c6b13d7c45241780a8446
takumi314 <isospin_symmetry@me.com>
2018-07-07 01:49:32 +0900

    [WIP] linking

---
+++ PageView-with-Container/ContainerViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ContainerViewController.swift
+//  PageView-with-Container
+//
+//  Created by NishiokaKohei on 2018/07/07.
+//  Copyright © 2018年 Takumi. All rights reserved.
+//
+
+import UIKit
+
+class ContainerViewController: UIViewController {
+
+    @IBOutlet private weak var titleScroller: UIScrollView!
+    @IBOutlet private weak var lineSegment: UISegmentedControl!
+    @IBOutlet private weak var contentScroller: UIScrollView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- PageView-with-Container/TopViewController.swift
+++ PageView-with-Container/TopViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TopViewController.swift
 //  PageView-with-Container
 //
 //  Created by NishiokaKohei on 2018/07/07.
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TopViewController: UIViewController {
 
+    @IBOutlet weak var containerView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
